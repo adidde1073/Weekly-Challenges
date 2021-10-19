601c7c073d2aa8135c3fc723d9701e6b24694b9c
Albert Didde <albertdidde@Alberts-MacBook-Pro.local>
2021-10-19 10:27:00 -0700

    Challenge #1 Javascript for week 10/18

---
+++ 10-18-track-robot.ts
@@ -0,0 +1,30 @@
+/* trackRobot(20, 30, 10, 40) --> [-10, 10]  */
+function trackRobot(...args: number[]) : number[] {
+    const moves: number[] = Array.from(args);
+    let direction: string = 'N';
+    let x: number = 0;
+    let y: number = 0;
+    for(let move of moves) {
+        switch(direction) {
+            case 'N': 
+                y += move;
+                direction = 'E';
+                break;
+            case 'E':
+                x += move;
+                direction = 'S';
+                break;
+            case 'S': 
+                y -= move;
+                direction = 'W';
+                break;
+            case 'W': 
+                x -= move;
+                direction = 'N';
+                break;
+        }
+    }
+    return [x, y];
+}
+
+console.log(trackRobot(20, 30, 10, 40));
